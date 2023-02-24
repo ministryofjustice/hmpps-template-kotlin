6452fda24817b147a23f65544ec241f9eb4999eb
Neil Mendum <neil.mendum@digital.justice.gov.uk>
2023-02-24 12:56:39 +0000

    Fixes for Gradle 8 and tidy ups
    https://github.com/ministryofjustice/hmpps-template-kotlin/pull/140 broke builder_docker (https://app.circleci.com/pipelines/github/ministryofjustice/hmpps-template-kotlin/1107/workflows/0f648224-0b09-4ae7-8a8f-f327927fcde5/jobs/2636)
    
    See 'Updated toolchain download repositories' on https://docs.gradle.org/8.0/release-notes.html

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,13 +12,13 @@ dependencies {
 }
 
 java {
-  toolchain.languageVersion.set(JavaLanguageVersion.of(18))
+  toolchain.languageVersion.set(JavaLanguageVersion.of(19))
 }
 
 tasks {
   withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
-      jvmTarget = "18"
+      jvmTarget = "19"
     }
   }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,5 @@
 rootProject.name = "hmpps-template-kotlin"
+
+plugins {
+  id("org.gradle.toolchains.foojay-resolver-convention") version("0.4.0")
+}
