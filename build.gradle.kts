80ea702271a7189350580e4db52bfb858b9935b4
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2024-06-26 16:03:14 +0100

    SDIT-1862: ⬆️ Upgrade to latest spring boot

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.1"
   kotlin("plugin.spring") version "2.0.0"
 }
 
