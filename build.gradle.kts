0e1ba39867592ba7ba7f8017e3a9c465466d2f83
james Hart <james.hart@digital.justice.gov.uk>
2025-08-28 17:20:33 +0100

    feat/HEAT-767 OWASP scan to v2 feed (#297)
    * add the new OWASP dependency check and location
    
    * set the nvd_feed_version to 2 so it uses the self-hosted runners
    
    * s'pose I'd better put it in quotes..
    
    * now using spring boot 9.0.0
    
    * HEAT-767: Upgrade to latest versions
    
    ---------
    
    Co-authored-by: Peter Phillips <5099053+petergphillips@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.3.4"
-  kotlin("plugin.spring") version "2.2.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.0.0"
+  kotlin("plugin.spring") version "2.2.10"
 }
 
 configurations {
@@ -8,13 +8,13 @@ configurations {
 }
 
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.4.11")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.5.0")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.11")
 
-  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.4.11")
+  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.5.0")
   testImplementation("org.wiremock:wiremock-standalone:3.13.1")
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.31") {
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.32") {
     exclude(group = "io.swagger.core.v3")
   }
 }
