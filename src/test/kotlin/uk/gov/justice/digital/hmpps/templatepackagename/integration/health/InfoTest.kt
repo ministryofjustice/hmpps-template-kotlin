3514dda1e92dd0a5cdb285142bcb3c7caf70206a
Kristian Gregory <kristian.gregory@digital.justice.gov.uk>
2026-05-19 06:54:06 +0100

    Fix infoPage int test that can cause flakiness if tests run on separate date to build version date

--- src/test/kotlin/uk/gov/justice/digital/hmpps/templatepackagename/integration/health/InfoTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/templatepackagename/integration/health/InfoTest.kt
@@ -2,11 +2,14 @@ package uk.gov.justice.digital.hmpps.templatepackagename.integration.health
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.info.BuildProperties
 import uk.gov.justice.digital.hmpps.templatepackagename.integration.IntegrationTestBase
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 
 class InfoTest : IntegrationTestBase() {
+
+  @Autowired
+  private lateinit var buildProperties: BuildProperties
 
   @Test
   fun `Info page is accessible`() {
@@ -25,7 +28,7 @@ class InfoTest : IntegrationTestBase() {
       .exchange()
       .expectStatus().isOk
       .expectBody().jsonPath("build.version").value<String> {
-        assertThat(it).startsWith(LocalDateTime.now().format(DateTimeFormatter.ISO_DATE))
+        assertThat(it).isEqualTo(buildProperties.version)
       }
   }
 }
